4d873b75ea2a4c90e922de4464eef7473fa31bec
brighamdent <hammy125@Brigham.localdomain>
2024-02-05 12:26:52 -0600

    update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 export default function DashBoard() {
+  console.log('hello')
   return (
     <div>
       <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
